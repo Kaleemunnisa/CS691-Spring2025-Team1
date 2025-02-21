8e0c59494eed9b0b779465682fbc01d4c522f1fe
livanve <149273269+livanve@users.noreply.github.com>
2025-02-20 19:22:21 -0500

    Add files via upload

---
+++ constants/__init__.py
@@ -0,0 +1,10 @@
+single_line_width = 8.23
+double_line_width = 10.97
+half_court_line_height = 11.88
+service_line_width = 6.4
+double_alley_difference = 1.37
+no_mans_land_height = 5.48
+
+#Change the following values to match the player heights for each clip
+player_1_height = 1.88 #Djolovic
+player_2_height = 1.91 #Sonego
